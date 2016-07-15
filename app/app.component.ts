1feaa24886d99550402fb3599adb5ed0d13af64a
Mark <mark.lishman@gmail.com>
2016-07-15 17:43:21 +0100

    Moved template examples to ne app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NgIfComponent } from "./ng-if.component";
-import { NgIfTemplateComponent } from "./ng-if-template.component";
 import { NgSwitchComponent } from "./ng-switch.component";
-import { NgSwitchTemplateComponent } from "./ng-switch-template.component";
 import { NgForComponent } from "./ng-for.component";
-import { NgForTemplateComponent } from "./ng-for-template.component";
 import { NgClassComponent } from "./ng-class.component";
 import { NgStyleComponent } from "./ng-style.component";
 
@@ -13,21 +10,15 @@ import { NgStyleComponent } from "./ng-style.component";
     selector: 'app',
     template: `
         <ng-if-directive *ngIf="showSection('ng-if')"></ng-if-directive>
-        <ng-if-template *ngIf="showSection('ng-if-template')"></ng-if-template>
         <ng-switch-directive *ngIf="showSection('ng-switch')"></ng-switch-directive>
-        <ng-switch-template *ngIf="showSection('ng-switch-template')"></ng-switch-template>
         <ng-for-directive *ngIf="showSection('ng-for')"></ng-for-directive>
-        <ng-for-template *ngIf="showSection('ng-for-template')"></ng-for-template>
         <ng-class-directive *ngIf="showSection('ng-class')"></ng-class-directive>
         <ng-style-directive *ngIf="showSection('ng-style')"></ng-style-directive>
         `,
     directives: [
         NgIfComponent,
-        NgIfTemplateComponent,
         NgSwitchComponent,
-        NgSwitchTemplateComponent,
         NgForComponent,
-        NgForTemplateComponent,
         NgClassComponent,
         NgStyleComponent
     ]
--- app/ng-if.component.ts
+++ app/ng-if.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, ViewChildren, QueryList, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'ng-if-directive',

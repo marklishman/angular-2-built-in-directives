994777d652c943ad75d53d121bf3d2e87b1b9e85
Mark <mark.lishman@gmail.com>
2016-07-16 10:57:13 +0100

    Condensed code

--- app/ng-for.component.ts
+++ app/ng-for.component.ts
@@ -4,18 +4,19 @@ import { Component } from '@angular/core';
     selector: 'ng-for-directive',
     template: `
         <h2>ngFor</h2>
-        <div *ngFor="let person of people; let i=index; let isOdd=odd; trackBy:trackByPeople">
-            <span [class.highlight]="isOdd">{{i}}. {{person.name.forename}} {{person.name.surname}}</span>
+        
+        <div *ngFor="let person of people; let isOdd=odd; trackBy:trackByPeople">
+            <span [class.highlight]="isOdd">{{person.name.forename}} {{person.name.surname}}</span>
         </div>
         
         <h3>For Loop</h3>
+        
         <span *ngFor="let item of forLoopArray(15); let i=index; let last=last">
             <span>{{i+1}}{{last ? '' : ','}}</span>
         </span>`,
     styles: ['.highlight {background-color: Lavender }']
 })
 export class NgForComponent {
-
     private people = [
             {id: 1, name: {forename: 'John', surname: 'Doe'}},
             {id: 2, name: {forename: 'John', surname: 'Smith'}},
@@ -26,7 +27,6 @@ export class NgForComponent {
     private trackByPeople(index: number, person: any) {
         return person.id;
     }
-
     private forLoopArray(elements: number): Array<any> {
         return new Array(elements);
     }

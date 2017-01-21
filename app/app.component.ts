6377c255fcfa5abe19e22de4ec0feee44fa2f39a
Mark <mark.lishman@gmail.com>
2017-01-21 14:28:33 +0000

    Latest version of Angular Removed  menu

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,17 +4,6 @@ import { Component } from '@angular/core';
     selector: 'app',
     template: `
         <div>
-            <p>
-                <a [routerLink]="['/ng-if']">ngIf</a> |
-                <a [routerLink]="['/show-hide']">show & hide</a> |
-                <a [routerLink]="['/ng-switch']">ngSwitch</a> |
-                <a [routerLink]="['/ng-for']">ngFor</a> |
-                <a [routerLink]="['/ng-class']">ngClass</a> |
-                <a [routerLink]="['/ng-style']">ngStyle</a>
-            </p>
-            <hr/>
-         </div>
-        <div>
             <router-outlet></router-outlet>
         </div>`,
 })
